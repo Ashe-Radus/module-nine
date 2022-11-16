f625507c9f6dc0a69ae365b63d012926ef75ade0
Scoripo <aradusewicz@gmail.com>
2022-11-15 22:11:53 -0700

    added video to readme file

--- README.md
+++ README.md
@@ -7,5 +7,8 @@ This project is designed to generate a README file in the intergrated terminal u
 To install and run the code, download the file and open the intergrated terminal. Run node index.js and answer the given prompts, once answered the file will generate a professional README file. 
 
 ## Links
-URL of git hub repository
-URL of deployed site
+URL of git hub repository; https://github.com/Ashe-Radus/module-nine
+
+
+## Demo 
+Untitled_ Nov 15, 2022 10_06 PM.webm
--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const questions = [
     {
         type: 'input',
         name: 'license',
-        message: 'Please choose a license for this project. ',
+        message: 'Please provide a license for this project. ',
         choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universial', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense']
     }
 ]
